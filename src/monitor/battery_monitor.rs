6d4c2367a7e4a95dbc1a1b40a187e51d4cba83e3
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-05-06 14:00:56 +0100

    chore(battery): changed battery message

--- src/monitor/battery_monitor.rs
+++ src/monitor/battery_monitor.rs
@@ -54,7 +54,7 @@ pub async  fn get_battery_info_pretty() -> Result<String, battery::Error> {
     );
 
     if percentage < 20.0 {
-        telegram::send_telegram_message(" 🪫 Battery is low!").await.expect("Failed to call telegram function on battery");
+        telegram::send_telegram_message(" 🪫 Your Mac Battery is low! Please charge 🔌").await.expect("Failed to call telegram function on battery");
     }
 
     Ok(report)
